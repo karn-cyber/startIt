16391d8a5ccd6a05ae83685223f6a65873a45d30
karn-cyber <karnneelanshu@gmail.com>
2025-07-25 11:30:46 +0530

    removed unnessacry lines

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import UserProfile from './pages/UserProfile';
 import Settings from './pages/Settings';
 
-// Add these routes to your existing routes
 <Route path="/profile/:userEmail" element={<UserProfile />} />
 <Route path="/settings" element={<Settings />} />
